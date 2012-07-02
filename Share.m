6a74f79a77de060e33183dcf97fe95291dc07c44
Joseph caxton-Idowu <Joseph@Joseph-caxton-Idowus-iMac.local>
2012-07-02 13:25:04 +0100

    Follow us

--- Share.m
+++ Share.m
@@ -56,7 +56,7 @@
 
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
 {
-    return 2;
+    return 3;
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
@@ -65,18 +65,26 @@
         
         return 1;
     }
-    else
+    else if (section == 1)
     {
         
         
         return 3;
         
     }
+    else if (section == 2){
+        
+        return 2;
+    }
+    
+    else {
+        return 0;
+    }
 }
 
 - (CGFloat) tableView:(UITableView *)tableView heightForRowAtIndexPath: (NSIndexPath *) indexPath {
     
-    if(indexPath.row == 0 && indexPath.section == 0){
+    if((indexPath.row == 0 && indexPath.section == 0) || (indexPath.row == 0 && indexPath.section == 2)){
         
         return 60;
     }
@@ -155,6 +163,32 @@
         cell.textLabel.text = @"Twitter";
         
     }
+    else if (indexPath.section == 2 && indexPath.row == 0){
+        
+        cell.textLabel.text = @"*** Very important you follow us on Twitter. We operate an alerts service through Twitter to keep you informed of any service interruptions.";
+        cell.textLabel.lineBreakMode = UILineBreakModeWordWrap;
+        cell.textLabel.numberOfLines = 0;
+        cell.textLabel.font = [UIFont fontWithName:@"Helvetica" size:14.0];
+        
+        
+        cell.selectionStyle = UITableViewCellSelectionStyleNone;
+        CGFloat nRed=236.0/255.0; 
+        CGFloat nBlue=207/255.0;
+        CGFloat nGreen=141.0/255.0;
+        UIColor *mycolor=[[UIColor alloc]initWithRed:nRed green:nBlue blue:nGreen alpha:1];
+        cell.contentView.backgroundColor = mycolor;
+        cell.textLabel.backgroundColor =  mycolor;
+        cell.textLabel.textColor = [UIColor blackColor];
+        cell.textLabel.textAlignment = UITextAlignmentCenter;
+        
+    }
+    else if (indexPath.section == 2 && indexPath.row == 1){
+        
+        UIImage* FollowUsOnTwitterImage = [UIImage imageNamed:@"FollowUsOnTwitter.png"];
+        cell.imageView.image = FollowUsOnTwitterImage;
+        cell.textLabel.text = @"Follow us on Twitter";
+    }
+
 
     
     return cell;
@@ -180,6 +214,12 @@
         
         [self Twit];
     }
+    
+    else if (indexPath.section == 2){
+        
+        [self FollowUsOnTwitter];
+    }
+
 }
 
 -(IBAction)ShareThisAppViaMail:(id)sender{
@@ -459,6 +499,25 @@
     }
 	
 }
+
+-(void)FollowUsOnTwitter{
+    
+    // Report to  analytics
+    NSError *error;
+    if (![[GANTracker sharedTracker] trackEvent:@"User sent to our twitter page to follow us"
+                                         action:@"User sent to our twitter page to follow us"
+                                          label:@"User sent to our twitter page to follow us"
+                                          value:1
+                                      withError:&error]) {
+        NSLog(@"error in trackEvent");
+    }
+    
+    
+    NSString *str = @"http://twitter.com/#!/learnerscloud"; 
+    [[UIApplication sharedApplication] openURL:[NSURL URLWithString:str]];
+    
+}
+
 
 
 
