c16e5aea36b048031b92218fe9b18cf7577a76a5
Joseph caxton-Idowu <Joseph@josephmacwin8.home>
2013-08-03 20:10:35 +0100

    fix

--- FreeVideosClass.h
+++ FreeVideosClass.h
@@ -20,8 +20,10 @@
     NSMutableArray *ProductsSubscibedTo;
     BOOL FullSubscription;
     UISearchBar  *mySearchBar;
-   
-    
+    NSMutableArray* buttons;
+    UIBarButtonItem *LoginViaLearnersCloud;
+    NSMutableArray *FreeSamples;
+    NSMutableArray *FreeSamples_Copy;
 }
 
 @property (nonatomic, retain) NSMutableArray *ArrayofConfigObjects;
@@ -31,6 +33,10 @@
 @property (nonatomic, retain)  NSMutableArray *ProductsSubscibedTo;
 @property (nonatomic, assign) BOOL FullSubscription;
 @property (nonatomic, retain) UISearchBar *mySearchBar;
+@property (nonatomic, retain) NSMutableArray* buttons;
+@property (nonatomic, retain) UIBarButtonItem *LoginViaLearnersCloud;
+@property (nonatomic, retain)  NSMutableArray *FreeSamples;
+@property (nonatomic, retain)  NSMutableArray *FreeSamples_Copy;
 
 
 
--- Start.m
+++ Start.m
@@ -150,8 +150,24 @@
 
     }
     
-   
+    // Check if we are suppose to show login
+    if(appDelegate.FlagToLoginOrLogout == [NSNumber numberWithInt:1]){
+        
+        appDelegate.FlagToLoginOrLogout = [NSNumber numberWithInt:0];
+       LoginViaLearnersCloud.title = @"Login";
+        [self TransferSubscription:LoginViaLearnersCloud];
+        
+    }
+    //Check if we are suppose to logout
+    else if (appDelegate.FlagToLoginOrLogout == [NSNumber numberWithInt:2]){
+        
+        appDelegate.FlagToLoginOrLogout = [NSNumber numberWithInt:0];
+       LoginViaLearnersCloud.title = @"Logout";
+        [self LogoutUser:LoginViaLearnersCloud];
+        
+    }
 
+  
        
 }
 
@@ -231,6 +247,14 @@
     UsernameText.enablesReturnKeyAutomatically = YES;
     [UsernameText setReturnKeyType:UIReturnKeyDone];
     [UsernameText setDelegate:self];
+        // Do we have stored UseName if so let add to box
+        NSUserDefaults *prefs = [NSUserDefaults standardUserDefaults];
+        NSString *StoredUserName = [prefs stringForKey:@"LCUserName"];
+        if ([StoredUserName length] > 1) {
+            UsernameText.text = StoredUserName;
+        }
+        
+
     [alertView addSubview:UsernameText];
     
     // Adds a password Field
@@ -543,7 +567,12 @@
                                                 withError:&error]) {
                   NSLog(@"error in trackEvent");
               }
-
+              //Store UserName for Later Use
+              
+              NSString *UsernameToStore = UsernameText.text;
+              NSUserDefaults *userDefaults = [NSUserDefaults standardUserDefaults];
+              [userDefaults setObject:UsernameToStore forKey:@"LCUserName"];
+              
               AppDelegate *appDelegate = (AppDelegate *)[UIApplication sharedApplication].delegate;
               appDelegate.AccessAll = TRUE;
               appDelegate.UserEmail = UsernameText.text;
