b2c835752a316d9a7a29e52f6e5ce203c88bd79c
josephcaxton <Josephcaxtonidowu@yahoo.com>
2014-04-08 14:46:04 +0100

    login fix

--- Help.m
+++ Help.m
@@ -180,13 +180,22 @@
         
         NSUserDefaults *prefs = [NSUserDefaults standardUserDefaults];
         NSString *DeviceID = [prefs stringForKey:@"LCUIID"];
-        
+        NSString *LCNOAuth = [prefs stringForKey:@"LCNOAuth"];
+        NSString *DeviceIDWhenPurchase = [prefs stringForKey:@"LCNOAuthDeviceID"];
         NSArray *SendTo = [NSArray arrayWithObjects:@"support@LearnersCloud.com",nil];
         
         MFMailComposeViewController *SendMailcontroller = [[MFMailComposeViewController alloc]init];
         SendMailcontroller.mailComposeDelegate = self;
         [SendMailcontroller setToRecipients:SendTo];
+        if([LCNOAuth isEqualToString:@"contactsupport"]){
+            
+            [SendMailcontroller setSubject:[NSString stringWithFormat:@"%@ / * %@ Maths video streaming iPhone",DeviceID,DeviceIDWhenPurchase]];
+        }
+        else{
+
+        
         [SendMailcontroller setSubject:[NSString stringWithFormat:@"%@ Maths video streaming iPhone",DeviceID]];
+        }
         
         [SendMailcontroller setMessageBody:[NSString stringWithFormat:@"Add Message here "] isHTML:NO];
         [self presentModalViewController:SendMailcontroller animated:YES];
